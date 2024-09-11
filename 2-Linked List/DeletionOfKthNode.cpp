b5b2447416b3dafc62d86e77a41f87d5841b5cdb
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-09-11 17:29:59 +0530

    Update DeletionOfKthNode.cpp

--- 2-Linked List/DeletionOfKthNode.cpp
+++ 2-Linked List/DeletionOfKthNode.cpp
@@ -38,19 +38,68 @@ void printList(Node *head)
         curr = curr->next;
     }
 }
-
+// function to delete the very first node of an linked list 
 Node* deleteHead(Node* head){
-    
+    if(head==nullptr){
+        return nullptr;
+    }
+    Node* temp=head;
+    head=head->next;
+    delete temp;
+    return head;
 }
 
 Node *deleteKthNode(Node *head, int k)
 {
+    // checking whether the given linked list is empty or not 
     if (head == nullptr)
     {
         return nullptr;
     }
+
+    //  if we need to delete the very first node or head of the linked list  we will call the function to delete the very first node of the linked list
     if (k == 0)
     {
-
+        return deleteHead(head);
     }
+    Node* temp=head;
+    // now we need to traverse just before   the node which we want to delete 
+    for(int i=0;i<k-1 && temp!=nullptr;i++){
+            temp=temp->next;
+    }
+
+
+    //  checking whether given node is in the list or not checking for out of bound 
+    if(temp==nullptr || temp->next ==nullptr){
+        return head;
+    }
+
+    Node* toDelete= temp->next;
+    temp->next= temp->next->next;
+    delete toDelete;
+    return head;
+
+}
+
+
+int main(){
+    int n, num;
+    Node* head= nullptr;  // initially we declare a empty linked list 
+
+    cout<<"Enter the number of nodes for the linked list :: ";
+    cin>>n;
+    cout<<"Enter the data for the nodes of the linked list :: ";
+    for(int i=0;i<n;i++){
+        cin>>num;
+        head=createNode(head,num);
+    }
+    cout<<"Printing the list before deletion of node at specified place :: ";
+    printList(head);
+    cout<<endl;
+    int ind;
+    cout<<"Enter the position of node which you want to delete from the linked list :: ";
+    cin>>ind;
+    cout<<"Printing the linked list after the deletion of the Node at the specified position :: ";
+    head=deleteKthNode(head,ind);
+    printList(head);
 }
